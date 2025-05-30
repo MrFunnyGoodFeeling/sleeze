575d3c26613a802e6c68abd91e9fdadf76031683
MrFunnyGoodFeeling <teddykinskywalker@gmail.com>
2025-05-31 01:56:16 +0200

    changed name on the layout from pineapple to wrapper

--- app/View/Components/Layouts/Wrapper.php
+++ app/View/Components/Layouts/Wrapper.php
@@ -6,7 +6,7 @@ use Closure;
 use Illuminate\Contracts\View\View;
 use Illuminate\View\Component;
 
-class Pineapple extends Component
+class Wrapper extends Component
 {
     /**
      * Create a new component instance.
@@ -21,6 +21,6 @@ class Pineapple extends Component
      */
     public function render(): View|Closure|string
     {
-        return view('components.layouts.pineapple');
+        return view('components.layouts.wrapper');
     }
 }
