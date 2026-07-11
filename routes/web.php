d35fb6dd5ef9e06005365d2bc98527cf8da94e33
MrFunnyGoodFeeling <teddykinskywalker@gmail.com>
2026-07-12 00:10:32 +0200

    There is no spoon

--- routes/web.php
+++ routes/web.php
@@ -1,19 +1,13 @@
 <?php
-
-use App\Http\Controllers\AdminController;
-use App\Http\Controllers\Auth\ReceptionController;
-use App\Http\Controllers\DashboardController;
-use App\Http\Controllers\WrapperController;
 use Illuminate\Support\Facades\Route;
 
 // Wrapper
 Route::middleware('guest', 'throttle:60,1')->group(function () {
-    Route::get('/', [WrapperController::class, 'landing'])->name('landing');
-    Route::get('/register', [ReceptionController::class, 'create'])->name('register');
-    Route::post('/register', [ReceptionController::class, 'store']);
-    Route::get('/login', [ReceptionController::class, 'login'])->name('login');
-    Route::post('/login', [ReceptionController::class, 'verifyLogin']);
-
+    Route::get('/', [App\Http\Controllers\WrapperController::class, 'landing'])->name('landing');
+    Route::get('/register', [App\Http\Controllers\Auth\ReceptionController::class, 'create'])->name('register');
+    Route::post('/register', [App\Http\Controllers\Auth\ReceptionController::class, 'store']);
+    Route::get('/login', [App\Http\Controllers\Auth\ReceptionController::class, 'login'])->name('login');
+    Route::post('/login', [App\Http\Controllers\Auth\ReceptionController::class, 'verifyLogin']);
     // Route::view('/register', 'auth.registration-closed')->name('register');
 });
 Route::middleware('throttle:100,1')->group(function () {
@@ -22,17 +16,16 @@ Route::middleware('throttle:100,1')->group(function () {
 
 // Dashboard (Auth)
 Route::middleware('auth', 'throttle:100,1')->group(function () {
-    Route::get('/dashboard', [DashboardController::class, 'home'])->name('dashboard');
-    Route::post('/logout', [ReceptionController::class, 'logout'])->name('logout');
+    Route::get('/dashboard', [App\Http\Controllers\DashboardController::class, 'home'])->name('dashboard');
+    Route::post('/logout', [App\Http\Controllers\ReceptionController::class, 'logout'])->name('logout');
 });
 
 // Admin (does not have throttle)
 Route::middleware('admin')->group(function () {
-    Route::get('/admin', [AdminController::class, 'index'])->name('admin');
+    Route::get('/admin', [App\Http\Controllers\AdminController::class, 'index'])->name('admin');
 });
 
 // Member
 Route::middleware('member', 'throttle:100,1')->group(function () {
-    // Route::get('/profile', [DashboardController::class, 'profile'])->name('profile');
-    Route::get('/settings', [DashboardController::class, 'settings'])->name('settings');
+    Route::get('/settings', [App\Http\Controllers\DashboardController::class, 'settings'])->name('settings');
 });
